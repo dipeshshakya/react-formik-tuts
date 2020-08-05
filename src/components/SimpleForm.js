688022e8a51f34eff1cf840cea2926682b1011ee
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-05 21:15:54 +1000

    validation function

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -9,6 +9,20 @@ function SimpleForm() {
     onSubmit: (values) => {
       console.log(values);
     },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) {
+        errors.name = "required";
+      }
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email format";
+      }
+      return errors;
+    },
   });
   console.log(formik.values);
   return (
