2bb4a600b548e4a7ab1d8caca8b3c991648c8001
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-05 20:54:32 +1000

    managing form state

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,15 +1,33 @@
 import React from "react";
-
+import { useFormik } from "formik";
 function SimpleForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+    },
+  });
+  console.log(formik.values);
   return (
     <div>
       <form>
         <label htmlFor="name">Name </label>
-        <input type="text" name="Name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
         <label htmlFor="email" id="email">
           Email
         </label>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
